6ecc7599db0b817cc787563ec397f3ab31b3ed84
kwonsubak <233090449+kwonsubak@users.noreply.github.com>
2026-01-07 23:01:39 +0900

    0107

--- sketch/World.js
+++ sketch/World.js
@@ -1,9 +1,15 @@
 class World {
   constructor() {
-    this.currentTime = 0;
+    this.current = [0, 0, 0];
+    this.initial = [0, 0, 0];
+    this.initial[0] = hour();
+    this.initial[1] = minute();
+    this.initial[2] = second();
     this.angle = 0;
     this.skyColour = null;
     this.GroundColour = null;
+    this.center = [width / 2, height / 2];
+    this.change = 0;
 
     this.morningColour = color("#b6e1f6");
     this.nightColour = color("#5d5da0");
@@ -13,35 +19,6 @@ class World {
     this.GroundColour = color("#a4dda4");
     this.GroundColour2 = color("#87c983");
     this.GroundColour3 = color("#558e72");
-
-    this.change = 0;
-  }
-
-  update() {
-    this.second = second();
-    this.minute = minute();
-    this.hour = hour();
-
-    this.currentMinute = this.minute + this.second / 60;
-    this.currentHour = this.hour / 24 + this.minute / 60 + this.second / 3600;
-
-    // 시간->각도 변환
-    this.secondAngle = map(this.second, 0, 60, 60, 360);
-    this.minuteAngle = map(this.currentMinute, 0, 60, 0, 360);
-    this.hourAngle = map(this.currentHour, 0, 24, 0, 360);
-
-    // if (this.hour >= 0 && this.hour <= 24) {
-    //   for (let i = 0; i < 12; i++) {
-    //     let correction = i * 30;
-    //     this.hourAngle = this.hourAngle + correction;
-    //   }
-    // }
-
-    //   if (this.currentHour >= 0 && this.currentHour < 24) {
-    //     this.change = map(this.currentHour, 0, 24, 0, 1);
-    //   } else {
-    //     this.change = map(this.currentHour + 24, 0, 24, 0, 1);
-    //   }
   }
 
   ground() {
@@ -67,84 +44,211 @@ class World {
     pop();
   }
 
-  secondArea() {
-    push();
-    translate(width / 2, height / 2 + 800);
-    rotate(radians(-this.second));
+  update() {
+    this.current[2] = (this.initial[2] + millis() / 1000) % 60;
+    this.current[1] = (this.initial[1] + millis() / (1000 * 60)) % 60;
+    this.current[0] = Math.floor(
+      (this.initial[0] + millis() / (1000 * 60 * 60)) % 24
+    );
 
-    let groundR = (width * 1.8) / 2;
-    let lineR = groundR - 50;
+    console.log(this.current[0], this.current[1], this.current[2]);
+    // this.second = second();
+    // this.minute = minute();
+    // this.hour = hour();
 
-    for (let i = 0; i <= 60; i++) {
-      push();
-      let rotationAngle = i * 6;
-      rotate(radians(rotationAngle));
-      translate(0, -lineR);
-      stroke("#F8F4EC");
-      strokeWeight(4);
-      line(0, 0, 0, -10);
-      pop();
-    }
-    pop();
+    // this.currentSecond = ((this.second * millis()) / 1000) % 60;
+    // this.currentMinute = ((this.second * millis()) / 6000) % 60;
+    // this.currentHour = ((this.hour * millis()) / 3600000) % 24;
+
+    // // 시간->각도 변환
+    // this.secondAngle = map(this.second, 0, 60, 60, 360);
+    // this.minuteAngle = map(this.currentMinute, 0, 60, 0, 360);
+    // this.hourAngle = map(this.currentHour, 0, 24, 0, 360);
+
+    // if (this.hour >= 0 && this.hour <= 24) {
+    //   for (let i = 0; i < 12; i++) {
+    //     let correction = i * 30;
+    //     this.hourAngle = this.hourAngle + correction;
+    //   }
+    // }
+
+    //   if (this.currentHour >= 0 && this.currentHour < 24) {
+    //     this.change = map(this.currentHour, 0, 24, 0, 1);
+    //   } else {
+    //     this.change = map(this.currentHour + 24, 0, 24, 0, 1);
+    //   }
+
+    this.drawWatch2(
+      this.center[0],
+      this.center[1] + 1000,
+      -this.sAngle(this.current[2] * 5),
+      680
+    );
+    this.drawWatch2(
+      this.center[0],
+      this.center[1] + 1000,
+      -this.mAngle(this.current[1]),
+      810
+    );
+    this.drawWatch(
+      this.center[0],
+      this.center[1] + 1000,
+      -this.hAngle(this.current[0] + this.current[1] / 60),
+      940
+    );
+    // console.log(`${this.current[0]}:${this.current[1]}:${this.current[2]}`);
   }
 
-  minuteArea() {
-    push();
-    translate(width / 2, height / 2 + 680);
-    rotate(radians(this.minuteAngle));
+  hAngle(time) {
+    return map(time % 12, 0, 12, 0, 360);
+  }
 
-    let groundR = (width * 1.9) / 2;
-    let lineR = groundR - 50;
+  mAngle(time) {
+    return map(time, 0, 60, 0, 360);
+  }
 
+  sAngle(time) {
+    return map(time, 0, 60, 0, 360);
+  }
+
+  drawWatch(x, y, angle, rad) {
     fill("#F8F4EC");
     textAlign(CENTER, CENTER);
-    textSize(50);
-    for (let i = 0; i <= 60; i++) {
+    push();
+    translate(x, y);
+    rotate(radians(angle));
+    textSize(rad * 0.03);
+    for (let n = 0; n < 12; n++) {
       push();
-      let rotationAngle = i * 6;
-      rotate(radians(rotationAngle));
-      translate(0, -lineR);
-      stroke("#F8F4EC");
-      strokeWeight(10);
-      line(0, 0, 0, -10);
+      const angle = (n * 360) / 12;
+      rotate(radians(angle));
+      const timeText = n == 0 ? 12 : n;
+      text(timeText, 0, -rad);
       pop();
     }
     pop();
   }
-
-  hourArea() {
-    push();
-    translate(width / 2, height / 2 + 550);
-    rotate(radians(this.hourAngle + 30));
-
-    let groundR = (width * 1.8) / 2;
-    let textR = groundR - 50;
-
+  drawWatch2(x, y, angle, rad) {
     fill("#F8F4EC");
     textAlign(CENTER, CENTER);
-    textSize(50);
-    for (let i = 1; i <= 12; i++) {
+    push();
+    translate(x, y);
+    rotate(radians(angle));
+    textSize(rad * 0.03);
+    for (let n = 0; n < 60; n++) {
       push();
-      let rotationAngle = i * 30;
-      rotate(radians(rotationAngle));
-      translate(0, -textR);
-      text(i, 0, 0);
-      pop();
-
-      push();
-      rotate(radians(rotationAngle));
-      translate(0, -textR);
-      stroke("#F8F4EC");
-      strokeWeight(10);
-      line(120, 0, 120, 0);
+      const angle = (n * 360) / 60;
+      rotate(radians(angle));
+      if (n % 5 === 0) {
+        text("I", 0, -rad);
+      } else {
+        text(".", 0, -rad);
+      }
       pop();
     }
     pop();
   }
+
+  // drawTicks(x, y, angle, rad) {
+  //   push();
+  //   translate(x, y);
+  //   rotate(radians(angle));
+  //   stroke("#F8F4EC");
+  //   for (let n = 0; n < 60; n++) {
+  //     push();
+  //     let tickAngle = n * 6;
+  //     rotate(radians(tickAngle));
+
+  //     if (n % 5 === 0) {
+  //       strokeWeight(6);
+  //       line(0, -rad, 0, -rad - 20);
+  //     } else {
+  //       strokeWeight(3);
+  //       line(0, -rad, 0, -rad - 10);
+  //     }
+  //     pop();
+  //   }
+  //   pop();
+  // }
+
+  // secondArea() {
+  //   push();
+  //   translate(width / 2, height / 2 + 800);
+  //   rotate(radians(-this.second));
+
+  //   let groundR = (width * 1.8) / 2;
+  //   let lineR = groundR - 50;
+
+  //   for (let i = 0; i <= 60; i++) {
+  //     push();
+  //     let rotationAngle = i * 6;
+  //     rotate(radians(rotationAngle));
+  //     translate(0, -lineR);
+  //     stroke("#F8F4EC");
+  //     strokeWeight(4);
+  //     line(0, 0, 0, -10);
+  //     pop();
+  //   }
+  //   pop();
+  // }
+
+  // minuteArea() {
+  //   push();
+  //   translate(width / 2, height / 2 + 680);
+  //   rotate(radians(this.minuteAngle));
+
+  //   let groundR = (width * 1.9) / 2;
+  //   let lineR = groundR - 50;
+
+  //   fill("#F8F4EC");
+  //   textAlign(CENTER, CENTER);
+  //   textSize(50);
+  //   for (let i = 0; i <= 60; i++) {
+  //     push();
+  //     let rotationAngle = i * 6;
+  //     rotate(radians(rotationAngle));
+  //     translate(0, -lineR);
+  //     stroke("#F8F4EC");
+  //     strokeWeight(10);
+  //     line(0, 0, 0, -10);
+  //     pop();
+  //   }
+  //   pop();
+  // }
+
+  // hourArea() {
+  //   push();
+  //   translate(width / 2, height / 2 + 550);
+  //   rotate(radians(this.hourAngle + 30));
+
+  //   let groundR = (width * 1.8) / 2;
+  //   let textR = groundR - 50;
+
+  //   fill("#F8F4EC");
+  //   textAlign(CENTER, CENTER);
+  //   textSize(50);
+  //   for (let i = 1; i <= 12; i++) {
+  //     push();
+  //     let rotationAngle = i * 30;
+  //     rotate(radians(rotationAngle));
+  //     translate(0, -textR);
+  //     text(i, 0, 0);
+  //     pop();
+
+  //     push();
+  //     rotate(radians(rotationAngle));
+  //     translate(0, -textR);
+  //     stroke("#F8F4EC");
+  //     strokeWeight(10);
+  //     line(120, 0, 120, 0);
+  //     pop();
+  //   }
+  //   pop();
+  // }
 
   dawn() {
     push();
-    this.change = map(this.currentHour, 4, 7, 0, 1);
+    this.change = map(this.current[0], 4, 7, 0, 1);
     this.skyColour = lerpColor(
       this.dawnColour,
       this.morningColour,
@@ -158,7 +262,7 @@ class World {
   morning() {
     //하늘
     push();
-    this.change = map(this.currentHour, 7, 17, 0, 1);
+    this.change = map(this.current[0], 7, 17, 0, 1);
     this.skyColour = lerpColor(
       this.morningColour,
       this.nightColour,
@@ -171,7 +275,7 @@ class World {
 
   evening() {
     push();
-    this.change = map(this.currentHour, 17, 19, 0, 1);
+    this.change = map(this.current[0], 17, 19, 0, 1);
     this.skyColour = lerpColor(
       this.eveningColour,
       this.nightColour,
@@ -185,20 +289,20 @@ class World {
   night() {
     //하늘
     push();
-    if (this.currentHour >= 19) {
+    if (this.current[0] >= 19) {
       this.skyColour = lerpColor(
         this.morningColour,
         this.nightColour,
         this.change
       );
-      this.change = map(this.currentHour, 19, 24, 0, 1);
+      this.change = map(this.current[0], 19, 24, 0, 1);
     } else {
       this.skyColour = lerpColor(
         this.nightColour,
         this.morningColour,
         this.change
       );
-      this.change = map(this.currentHour, 0, 4, 0, 1);
+      this.change = map(this.current[0], 0, 4, 0, 1);
     }
     fill(this.skyColour);
     rect(0, 0, width, height);
@@ -206,21 +310,17 @@ class World {
   }
 
   render() {
-    this.update();
-
-    if (this.hour >= 4 && this.hour < 7) {
+    if (this.current[0] >= 4 && this.current[0] < 7) {
       this.dawn();
-    } else if (this.hour >= 7 && this.hour < 17) {
+    } else if (this.current[0] >= 7 && this.current[0] < 17) {
       this.morning();
-    } else if (this.hour >= 17 && this.hour < 19) {
+    } else if (this.current[0] >= 17 && this.current[0] < 19) {
       this.evening();
     } else {
       this.night();
     }
 
     this.ground();
-    this.secondArea();
-    this.minuteArea();
-    this.hourArea();
+    this.update();
   }
 }
